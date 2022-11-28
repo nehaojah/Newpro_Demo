8200b06da20d2e10af8c51341bb98cb00bb95ab8
nehaojah <neha.fsm@gmail.com>
2022-11-28 13:12:58 +0100

    commit 2

---
+++ src/firstTry1/java2try.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package firstTry1;
+
+/**
+ * @author Neha
+ *
+ */
+public class java2try {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
