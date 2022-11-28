844e2337430d82e6119a2fd36de218e90bc571fa
nehaojah <neha.fsm@gmail.com>
2022-11-28 12:50:51 +0100

    new commit on 28 nov

---
+++ src/firstTry1/JavaExamples.java
@@ -0,0 +1,25 @@
+package firstTry1;
+
+public class JavaExamples {
+
+	int i = 5;
+	int[] arr = { 2, 3, 4, 5 };
+
+	int add(int a, int b) {
+		return a + b;
+	}
+
+	public void display() {
+		System.out.println("hi");
+	}
+
+	public static void main(String[] args) {
+
+		JavaExamples obj = new JavaExamples();
+		System.out.println(obj.i);
+		System.out.println(obj.add(9, 1));
+		// TODO Auto-generated method stub
+
+	}
+
+}
